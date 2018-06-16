0914b559462e7d49e479bb2f064ddee907cd448a
Jonic Linley <jonic@codeclub.org.uk>
2018-06-16 21:18:12 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-ZIP_NAME="knifey-spoony.zip"
+GAME="knifey-spoony"
+BIN_DIR="./${GAME}.bin"
+BUILD_DIR="./build"
+LINUX_DIR="${BIN_DIR}/linux"
+WINDOWS_DIR="${BIN_DIR}/windows"
+
+LINUX_FILES="${LINUX_DIR}/data.pod ${LINUX_DIR}/${GAME} ${LINUX_DIR}/${GAME}.png"
+MACOS_APP="./${GAME}.bin/${GAME}.app"
+WINDOWS_FILES="${WINDOWS_DIR}/data.pod ${WINDOWS_DIR}/${GAME}.exe ${WINDOWS_DIR}/SDL2.dll"
+WEB_FILES="./index.html ./${GAME}.js ./web/**/*"
 
 clear
 
@@ -10,17 +19,32 @@ echo " )  ( /    / )(  ) _)  ) _)  )  /   \___ \ ) __/(  O )(  O )/    / )  / "
 echo "(__\_)\_)__)(__)(__)  (____)(__/    (____/(__)   \__/  \__/ \_)__)(__/  "
 echo ""
 
-if [ -f $ZIP_NAME ]; then
-  echo "${ZIP_NAME} exists - removing..."
-  rm $ZIP_NAME
-  echo " - Done!"
-  echo ""
+if [ -d "./build" ]; then
+  rm -rf ./build
 fi
 
-echo "Creating: ${ZIP_NAME}"
-zip $ZIP_NAME ./index.html ./knifey-spoony.js ./web/**/*
+mkdir $BUILD_DIR
+chmod -R 775 $BUILD_DIR
+
+echo "Creating: ${GAME}-html.zip"
+zip "${BUILD_DIR}/${GAME}-html.zip" -r $WEB_FILES
 echo ""
+
+echo "Creating: ${GAME}-linux.zip"
+zip -j "${BUILD_DIR}/${GAME}-linux.zip" $LINUX_FILES
+echo ""
+
+echo "Creating: ${GAME}-macos.zip"
+zip -r "${BUILD_DIR}/${GAME}-macos.zip" $MACOS_APP
+echo ""
+
+echo "Creating: ${GAME}-windows.zip"
+zip -j "${BUILD_DIR}/${GAME}-windows.zip" $WINDOWS_FILES
+echo ""
+
 echo "All done!"
 echo ""
-open .
+
+open ./build
+
 exit 0
